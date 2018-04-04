3c97afb7265b06806a350bb42c2b7ddc3171a55e
Mateusz Michalak <michalakweb@gmail.com>
2018-04-04 14:43:50 +0700

    - added images at the bottom of the screen

--- wordpress/wp-content/themes/blog/footer.php
+++ wordpress/wp-content/themes/blog/footer.php
@@ -2,6 +2,12 @@
 
 </main><!-- /.container -->
 
+<img id="puppetL" class="d-none d-lg-block" src="<?php echo get_template_directory_uri(); ?>/images/puppet1.png" alt="puppet decoration image">
+
+<div class="d-none d-lg-block" id="flagimage">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/chineseflag.png" alt="puppet decoration image">
+</div>
+
 
 <footer class="blog-footer">
 	<!--<p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>-->
@@ -10,15 +16,6 @@
 	</p>
 </footer>
 
-
-<script>
-	Holder.addTheme('thumb', {
-		bg: '#55595c',
-		fg: '#eceeef',
-		text: 'Thumbnail'
-	});
-</script>
-<script src="http://localhost/wp-content/themes/blog/scripts/script.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
