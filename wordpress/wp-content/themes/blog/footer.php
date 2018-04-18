df720d31a744c6612c6de972693fb5639c58b1f6
Mateusz Michalak <michalakweb@gmail.com>
2018-04-18 22:32:06 +0700

    - customized footer

--- wordpress/wp-content/themes/blog/footer.php
+++ wordpress/wp-content/themes/blog/footer.php
@@ -12,7 +12,7 @@
 <footer class="blog-footer">
 	<!--<p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>-->
 	<p>
-		<a href="#">Powrót na górę</a>
+		Copyright © <?php echo date('Y') ?> · Travelazja
 	</p>
 </footer>
 
