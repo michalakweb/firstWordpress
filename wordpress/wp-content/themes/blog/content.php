674c61eda7c8da746f083ef288ca055570910a68
Mateusz Michalak <michalakweb@gmail.com>
2018-04-18 22:45:00 +0700

    - brushed up post section

--- wordpress/wp-content/themes/blog/content.php
+++ wordpress/wp-content/themes/blog/content.php
@@ -1,6 +1,12 @@
 <div class="blog-post">
 	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	<p class="blog-post-meta">
+		<?php the_date('d-m-Y'); ?> · <!--by <a href="#"><?php the_author(); ?>-->
+		<a id="komentarz" href="<?php comments_link(); ?>">
+			<?php
+			printf( _nx('1 Komentarz', 'Komentarze (%1$s)', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
+		</a>
+	</p>
 	
 	<div class="img-responsive-16by9">
 			<?php echo the_post_thumbnail('my-mainpage-thumb'); ?>
@@ -8,10 +14,7 @@
 	
 	<?php the_excerpt(); ?>
 	
-	<a href="<?php comments_link(); ?>">
-		<?php
-		printf( _nx('1 Komentarz', 'Komentarze (%1$s)', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
-	</a>
+	
 	
 </div><!-- /.blog-post -->
 
