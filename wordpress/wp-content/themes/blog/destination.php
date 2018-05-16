7b2fcfb944b9c9e001f15b7645384171630022d8
Mateusz Michalak <michalakweb@gmail.com>
2018-05-16 14:05:52 +0700

    - thai destination shows as a list on mobile

--- wordpress/wp-content/themes/blog/destination.php
+++ wordpress/wp-content/themes/blog/destination.php
@@ -7,8 +7,30 @@
 		<div class="container-fluid destLoop">
 			<h3 class="mt-4 mb-3">Z pamiętnika:</h3>
 			
+			<ul class="list-group list-group-flush d-md-none text-dark">
+				<?php
+				$catThailand = new WP_Query('cat=9&tag=pamietnik&orderby=title&order=ASC');
+
+				if ( $catThailand->have_posts() ) : while ($catThailand->have_posts() ) : $catThailand->the_post();
+				?>
 			
-			<div class="row justify-content-center">
+				<li class="list-group-item"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+				
+				<?php
+				endwhile; 
+				?>
+
+				<?php else : ?>
+
+				<?php _e('Sorry, no posts matched your criteria.'); ?>
+
+				<?php endif; ?>
+
+				<?php wp_reset_postdata(); ?>
+			</ul>
+			
+			
+			<div class="row justify-content-center d-none d-md-flex">
 					<?php
 					$catThailand = new WP_Query('cat=9&tag=pamietnik&orderby=title&order=ASC');
 
@@ -32,12 +54,6 @@
 					endwhile; 
 					?>
 
-					<div class="nav-prev"><?php next_posts_link('Starsze'); ?>
-					</div>
-
-					<div class="nav-next alignright"><?php previous_posts_link( 'Nowsze' ); ?></div>
-
-
 					<?php else : ?>
 
 					<?php _e('Sorry, no posts matched your criteria.'); ?>
@@ -50,46 +66,61 @@
 		<h3 class="mt-4 mb-3">Praktyczne:</h3>
 
 
-			<div class="row justify-content-center mb-4">
-				<?php
-				$catThailand = new WP_Query('tag=praktyczne');
+				<ul class="list-group list-group-flush rounded d-md-none text-dark">
+					<?php
+					$catThailand = new WP_Query('tag=praktyczne');
 
-				if ( $catThailand->have_posts() ) : while ($catThailand->have_posts() ) : $catThailand->the_post();
-				?>
+					if ( $catThailand->have_posts() ) : while ($catThailand->have_posts() ) : $catThailand->the_post();
+					?>
 
-				<div class="col-auto mb-2">
-					<div class="card text-dark" style="width: 15rem;">
-						<a href="<?php the_permalink(); ?>">
-							<img class="card-img-top" src=<?php echo the_post_thumbnail('my-mainpage-thumb'); ?>
-						</a>
-						<div class="card-body">
-							<h6 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+					<li class="list-group-item"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+
+					<?php
+					endwhile; 
+					?>
+
+					<?php else : ?>
+
+					<?php _e('Sorry, no posts matched your criteria.'); ?>
+
+					<?php endif; ?>
+
+					<?php wp_reset_postdata(); ?>
+				</ul>
+
+
+				<div class="row justify-content-center d-none d-md-flex">
+					<?php
+					$catThailand = new WP_Query('tag=praktyczne');
+
+					if ( $catThailand->have_posts() ) : while ($catThailand->have_posts() ) : $catThailand->the_post();
+					?>
+
+					<div class="col-auto mb-2">
+						<div class="card text-dark" style="width: 15rem;">
+							<a href="<?php the_permalink(); ?>">
+								<img class="card-img-top" src=<?php echo the_post_thumbnail('my-mainpage-thumb'); ?>
+									 </a>
+
+								<div class="card-body">
+									<h6 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+								</div>
+								</div>
 						</div>
+
+
+						<?php
+						endwhile; 
+						?>
+
+						<?php else : ?>
+
+						<?php _e('Sorry, no posts matched your criteria.'); ?>
+
+						<?php endif; ?>
+
+						<?php wp_reset_postdata(); ?>
 					</div>
-				</div>
-
-
-			<?php
-			endwhile; 
-			?>
-
-			<div class="nav-prev"><?php next_posts_link('Starsze'); ?>
-			</div>
-
-			<div class="nav-next alignright"><?php previous_posts_link( 'Nowsze' ); ?></div>
-
-
-			<?php else : ?>
-
-			<?php _e('Sorry, no posts matched your criteria.'); ?>
-
-			<?php endif; ?>
-
-			<?php wp_reset_postdata(); ?>
-
-
-
-		</div>
 			
 		</div>
 
