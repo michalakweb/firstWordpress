92b71a896052b94bf809c185ed935611a2c54590
Mateusz Michalak <michalakweb@gmail.com>
2018-05-16 13:04:28 +0700

    - added flexcards and 2 different loops on thai destination

--- wordpress/wp-content/themes/blog/destination.php
+++ wordpress/wp-content/themes/blog/destination.php
@@ -5,39 +5,87 @@
 <main role="main" class="container-fluid thaiBg">
 	<div class="row">
 		<div class="container-fluid destLoop">
-			<div class="col-md-8 blog-main">
-				
-				<p><?php echo get_cat_name(9); ?></p>
+			<h3>Z pamiętnika:</h3>
+			
+			
+			<div class="row">
+					<?php
+					$catThailand = new WP_Query('cat=9');
 
+					if ( $catThailand->have_posts() ) : while ($catThailand->have_posts() ) : $catThailand->the_post();
+					?>
+
+				<div class="col mb-1">
+					<div class="card text-dark" style="width: 15rem;">
+						<img class="card-img-top" src=<?php echo the_post_thumbnail('my-mainpage-thumb'); ?>
+						<div class="card-body">
+							<h6 class="card-title"><?php the_title(); ?></h6>
+						</div>
+					</div>
+				</div>
+
+
+					<?php
+					endwhile; 
+					?>
+
+					<div class="nav-prev"><?php next_posts_link('Starsze'); ?>
+					</div>
+
+					<div class="nav-next alignright"><?php previous_posts_link( 'Nowsze' ); ?></div>
+
+
+					<?php else : ?>
+
+					<?php _e('Sorry, no posts matched your criteria.'); ?>
+
+					<?php endif; ?>
+					
+			        <?php wp_reset_postdata(); ?>
+			</div>
+			
+			<h3>Praktyczne:</h3>
+
+
+			<div class="row">
 				<?php
 				$catThailand = new WP_Query('cat=9');
 
 				if ( $catThailand->have_posts() ) : while ($catThailand->have_posts() ) : $catThailand->the_post();
 				?>
-				
-				
 
-				<h2><?php the_title(); ?></h2>
-
-
-				<?php
-				endwhile; 
-				?>
-
-				<div class="nav-prev"><?php next_posts_link('Starsze'); ?>
+				<div class="col mb-1">
+					<div class="card text-dark" style="width: 15rem;">
+						<img class="card-img-top" src=<?php echo the_post_thumbnail('my-mainpage-thumb'); ?>
+							 <div class="card-body">
+						<h6 class="card-title"><?php the_title(); ?></h6>
+					</div>
 				</div>
-
-				<div class="nav-next alignright"><?php previous_posts_link( 'Nowsze' ); ?></div>
-
-
-				<?php else : ?>
-
-				<?php _e('Sorry, no posts matched your criteria.'); ?>
-
-				<?php endif; ?>
+			</div>
 
 
-			</div><!-- /.blog-main -->
+			<?php
+			endwhile; 
+			?>
+
+			<div class="nav-prev"><?php next_posts_link('Starsze'); ?>
+			</div>
+
+			<div class="nav-next alignright"><?php previous_posts_link( 'Nowsze' ); ?></div>
+
+
+			<?php else : ?>
+
+			<?php _e('Sorry, no posts matched your criteria.'); ?>
+
+			<?php endif; ?>
+
+			<?php wp_reset_postdata(); ?>
+
+
+
+		</div>
+			
 		</div>
 
 		<?php get_footer(); ?>
