cd2aedae0d89086b9890aaf748806412404718b6
Mateusz Michalak <michalakweb@gmail.com>
2018-05-22 14:56:57 +0700

    - added a link in the header to practical

--- wordpress/wp-content/themes/blog/header.php
+++ wordpress/wp-content/themes/blog/header.php
@@ -43,7 +43,7 @@
 						</div>
 					</li>
 					<li class="nav-item active">
-						<a class="nav-link" href="#">Praktyczne</a>
+						<a class="nav-link" href="<?php echo get_page_link(); ?>">Praktyczne</a>
 					</li>
 					<li class="nav-item active">
 						<a class="nav-link" href="#">O mnie</a>
